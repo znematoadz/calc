93469f4d23b4242ac055800aa1293246f436d508
Alex J <jones6211@gmail.com>
2018-11-21 21:35:10 -0600

    Updates

---
+++ precache-manifest.8492c37302ac8aea1aa11ba0d342b31b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "117ab63626df48b19938",
+    "url": "/calc/static/js/runtime~main.117ab636.js"
+  },
+  {
+    "revision": "4b933434aa14a24088e5",
+    "url": "/calc/static/js/main.4b933434.chunk.js"
+  },
+  {
+    "revision": "667b3c48161086f71678",
+    "url": "/calc/static/js/1.667b3c48.chunk.js"
+  },
+  {
+    "revision": "4b933434aa14a24088e5",
+    "url": "/calc/static/css/main.a6122f37.chunk.css"
+  },
+  {
+    "revision": "667b3c48161086f71678",
+    "url": "/calc/static/css/1.58cf0515.chunk.css"
+  },
+  {
+    "revision": "fb1d6ef31198f4ad22e33e64e2b0f821",
+    "url": "/calc/index.html"
+  }
+];
